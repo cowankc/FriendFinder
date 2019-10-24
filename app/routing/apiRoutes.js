0fd61c2212c4430bc5b53923d91bd4054684dbc7
cowankc <cowankc92@live.com>
2019-10-24 17:28:16 -0500

    created ajax post to match friend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -7,13 +7,24 @@ app.get("/api/friends", function(req, res) {
   });
 
 app.post("/api/friends", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    let newfriend = req.body;
-    console.log(newfriend);  
-    // We then add the json the user sent to the character array
-    friends.push(newfriend);
-    // We then display the JSON to the users
-    res.json(newfriend);
+    let newUser = req.body;
+    let userAnswers = newUser.scores;
+    let friendMatchName = "";
+    let friendMatchImage = "";
+    let totalDifference = 1000
+
+    for (let i = 0; i < friends.length; i++) {
+      let scoreDifference = 0;
+      for (var x; x < userAnswers.length; x++) {
+        scoreDifference += Math.abs(friends[i].scores[x] - userAnswers[x]);
+      }
+      if (scoreDifference < totalDifference) {
+        totalDifference = scoreDifference;
+        friendMatchName = friends[i].name;
+        friendMatchImage = friends[i].photo;
+      }
+    }
+    friends.push(newUser);
+    res.json({friendMatchName: friendMatchName, friendMatchImage: friendMatchImage})
   });
 }
