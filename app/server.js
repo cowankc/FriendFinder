8467329774e15ec9d2dd56232f1840b7be1028cc
cowankc <cowankc92@live.com>
2019-10-26 21:47:13 -0500

    fixed port to be able to be deployed by heroku

--- app/server.js
+++ app/server.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let path = require("path");
 let app = express();
-let PORT = 3000;
+let PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
