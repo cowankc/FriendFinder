3b079d5e7b4470ca38b6098955b81538db9db032
cowankc <cowankc92@live.com>
2019-10-26 22:00:15 -0500

    moved server.js file to root folder

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ let PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require(path.join(__dirname, './routing/apiRoutes'))(app);
-require(path.join(__dirname, './routing/htmlRoutes'))(app);
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
